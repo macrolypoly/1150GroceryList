a7c949e41ec11687dc6dafde173a546cc3f91e57
Michael Cotofan <mccotofan@gmail.com>
2021-05-07 22:25:05 -0400

    Added-IngredientList-ServicesAndModels

--- 1150GroceryList.Data/IngredientList.cs
+++ 1150GroceryList.Data/IngredientList.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,13 @@ namespace _1150GroceryList.Data
 {
    public class IngredientList
     {
-
+        [Key]
+        public int IngredientListID { get; set; }
+        [ForeignKey("Recipe")]
+        public int RecipeId { get; set; }
+        public virtual Recipe Recipe { get; set; }
         public List<Ingredient> ListOfIngredients { get; set; }
+
 
     }
 }
