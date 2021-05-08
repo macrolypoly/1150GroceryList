1b7ba3cba8207a4f01b6491a354c106271b7d3ba
Nasser <adegboyefoluso@yahoo.com>
2021-05-08 12:43:19 -0400

    INgredientLisstModel

---
+++ 1150GroceryList.Models/IngredientListModel/IngredientDataListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150GroceryList.Models.IngredientListModel
+{
+    public class IngredientDataListItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
