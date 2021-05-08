ed8fff147e9f3a160544fb1705a3864689ac7a46
Nasser <adegboyefoluso@yahoo.com>
2021-05-08 09:33:35 -0400

    CommitIngredientListTable

--- 1150GroceryList.Data/IngredientList.cs
+++ 1150GroceryList.Data/IngredientList.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _1150GroceryList.Data
 {
-   public class IngredientList
+    public class IngredientList
     {
 
-        public List<Ingredient> ListOfIngredients { get; set; }
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public virtual ICollection<Ingredient> ListofIngredient { get; set; }
+
+        public IngredientList()
+        {
+            ListofIngredient = new HashSet<Ingredient>();
+            
+        }
 
     }
 }
