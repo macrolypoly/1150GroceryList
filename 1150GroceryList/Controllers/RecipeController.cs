96a55f880ac46c85782a876e601ab417b1d0419c
Max Hahn <max.vhahn@gmail.com>
2021-05-08 15:48:59 -0400

    Updated Recipe Contorller

--- 1150GroceryList/Controllers/RecipeController.cs
+++ 1150GroceryList/Controllers/RecipeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _1150GroceryList.Models;
+using _1150GroceryList.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,57 @@ using System.Web.Http;
 
 namespace _1150GroceryList.Controllers
 {
+    [Authorize]
     public class RecipeController : ApiController
     {
+        public IHttpActionResult Get(int id)
+        {
+            RecipeService recipeService = CreateRecipeService();
+            var recipe = recipeService.GetRecipeById(id);
+            return Ok(recipe);
+        }
+
+        public IHttpActionResult Post(RecipeCreate recipe)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRecipeService();
+
+            if (!service.CreateRecipe(recipe))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateRecipeService();
+
+            if (!service.DeleteRecipe(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private RecipeService CreateRecipeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var recipeService = new RecipeService(userId);
+            return recipeService;
+        }
+
+        //public class ReplyService
+        //{
+        //    private readonly Guid _userId;
+        //    public ReplyService(Guid userId)
+        //    {
+        //        _userId = userId;
+        //    }
+
+        //    public Create
+        //}
     }
+
+    
 }
