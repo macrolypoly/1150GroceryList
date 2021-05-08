e8a8cd2c8fc0693731f21924bfbec5ad196583a2
Max Hahn <max.vhahn@gmail.com>
2021-05-07 20:20:03 -0400

    added properties to recipe class

---
+++ 1150GroceryList.Data/IngredientList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150GroceryList.Data
+{
+   public class IngredientList
+    {
+        public List<Ingredient> ListOfIngredients { get; set; }
+    }
+}
---
+++ 1150GroceryList.Data/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150GroceryList.Data
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public Guid OwnerId { get; set; }
+        public string RecipeDescription { get; set; }
+        public IngredientList IngredientList { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
