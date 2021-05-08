21a3c79fbea668a72a6f22e705c0c094944bf492
Max Hahn <max.vhahn@gmail.com>
2021-05-08 10:13:14 -0400

    Model and Service built

---
+++ 1150GroceryList.Models/RecipeCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150GroceryList.Models
+{
+    public class RecipeCreate
+    {
+        public int RecipeId { get; set; }
+        public string RecipeDescription { get; set; }
+    }
+}
---
+++ 1150GroceryList.Services/RecipeService.cs
@@ -0,0 +1,31 @@
+﻿using _1150GroceryList.Data;
+using _1150GroceryList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150GroceryList.Services
+{
+    public class RecipeService
+    {
+        public bool CreateRecipe(RecipeCreate model)
+        {
+            var entity =
+                new Recipe()
+                {
+                    RecipeId = model.RecipeId,
+                    RecipeDescription = model.RecipeDescription
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Recipes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+
+    }
+}
